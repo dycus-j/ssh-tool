6095d5dd99d544570cb4771de110284791dff9bd
James Dycus <dycus.j.jd@gmail.com>
2025-07-23 17:54:20 -0400

    added save_output_to_file function

--- ssh-tool.py
+++ ssh-tool.py
@@ -2,6 +2,7 @@ import paramiko
 import getpass
 import sys
 import time
+import os
 
 def get_hostname():
     # Gathers and validates hostname from command-line arguements.
@@ -11,14 +12,14 @@ def get_hostname():
         print("Example: python3 ssh_tool.py 192.168.64.1")
         sys.exit(1)
         
-        return sys.argv[1]
+    return sys.argv[1]
         
     
 
 def get_credentials(host):
     # Gathers connection credentials
 
-    username = input(f"Enter username for {host} [default: your_username]: ")
+    username = input(f"Enter username for {host}: ")
     if not username:
         print("Error: Username cannot be empty.")
         sys.exit(1)
@@ -34,7 +35,7 @@ def create_connection(host, username, password):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
     try:
-        print(f"\n[+] Connecting to {hostname}...")
+        print(f"\n[+] Connecting to {host}...")
         client.connect(hostname=host, username=username, password=password)
         print("✓ Connection successful!")
         return client
@@ -44,6 +45,8 @@ def create_connection(host, username, password):
     
     
 def exec_command(ssh_client, command):
+    # Executes cli commands for the client
+    
     if not ssh_client:
         return None
     
@@ -57,7 +60,29 @@ def exec_command(ssh_client, command):
     
     return stdout.read().decode()
 
+
+def save_output_to_file(host, command_output):
+    # saves the given output of a command to a timestamped backup file
+    
+    if not command_output:
+        return
+    
+    backup_dir = 'backups'
+    
+    if not os.path.exists(backup_dir):
+        os.makedirs(backup_dir)
         
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    filename = f"backup_{host}_{timestamp}.txt"
+    
+    backup_dir_path = os.path.join(backup_dir, filename)
+    
+    
+    with open(backup_dir_path, "w") as f:
+        f.write(command_output)
+    print(f"\n✓ Output successfully saved to '{backup_dir_path}'")
+    return
+    
         
 def main():
     # Get hostname from CLI
@@ -73,3 +98,13 @@ def main():
     cli_command = "ip a"
     command_output = exec_command(client ,cli_command)
     
+    # Save command output
+    save_output_to_file(host, command_output)
+    
+    # Close SSH connection
+    if client:
+        client.close()
+    
+    
+if __name__ == "__main__":
+    main()
